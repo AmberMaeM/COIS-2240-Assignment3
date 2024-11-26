82fc98a0541e3724baa93532dcb8b38a03de79c4
Amber Mae <ambermae.m@bell.net>
2024-11-26 15:31:34 -0500

    Refactored Transaction class to Singleton and updated LibraryManagement(Q1).

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,9 +1,49 @@
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Transaction {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static Transaction transactionInstance;//singleton
+	private Transaction() {
+		
 	}
+	//methiod to get single instance of transaction
+	public static Transaction getTransaction() {
+		if (transactionInstance == null) {
+			transactionInstance = new Transaction();
+		}
+		return transactionInstance;
+	}
+	
+    // Perform the borrowing of a book
+    public boolean borrowBook(Book book, Member member) {
+        if (book.isAvailable()) {
+            book.borrowBook();
+            member.borrowBook(book); 
+            String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
+            System.out.println(transactionDetails);
+            return true;
+        } else {
+            System.out.println("The book is not available.");
+            return false;
+        }
+    }
 
+    // Perform the returning of a book
+    public void returnBook(Book book, Member member) {
+        if (member.getBorrowedBooks().contains(book)) {
+            member.returnBook(book);
+            book.returnBook();
+            String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
+            System.out.println(transactionDetails);
+        } else {
+            System.out.println("This book was not borrowed by the member.");
+        }
+    }
+
+    // Get the current date and time in a readable format
+    private static String getCurrentDateTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(new Date());
+    }
 }
